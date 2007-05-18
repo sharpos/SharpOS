12ec99db5d8c24f8177557723bffc675c65f9b3d
Darx Kies <darxkies@gmail.com>
2007-05-18 23:53:25 +0000

    Kernel.Tests refactoring, changing the build scripts and adding more test cases. As the AOT (thanks to xfury) can generate a kernel image using more than one assembly the Kernel.Tests is split in 3 .dll: the main part is the mini kernel that runs the tests and displays the errors. The other two are test cases written in IL or in C#. That's right. ;) Gentleman, start your engines and help making the SharpOS AOT more robust with test cases either in C# or IL. :D

---
+++ SharpOS.Kernel.Tests/Kernel.cs
@@ -0,0 +1,58 @@
+// 
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Mircea-Cristian Racasan <darx_kies@gmx.net>
+//
+// Licensed under the terms of the GNU GPL License version 2.
+//
+
+using System;
+using System.Runtime.InteropServices;
+using SharpOS;
+using SharpOS.AOT.X86;
+using SharpOS.AOT.IR;
+
+namespace SharpOS {
+	public unsafe partial class KRNL {
+		[SharpOS.AOT.Attributes.KernelMain]
+		public unsafe static void BootEntry (uint magic, uint pointer, uint kernelStart, uint kernelEnd)
+		{
+			Screen.SetAttributes (Screen.ColorTypes.Yellow, Screen.ColorTypes.Black);
+
+			Screen.WriteLine (String ("SharpOS Unit Tests (http://www.sharpos.org)"));
+			Screen.WriteNL ();
+
+			RunTests ();
+			
+			while (true);
+		}
+
+		#region Stubs
+		[SharpOS.AOT.Attributes.String]
+		public unsafe static byte* String (string value)
+		{
+			return null;
+		}
+
+		[SharpOS.AOT.Attributes.Alloc]
+		public unsafe static byte* Alloc (uint value)
+		{
+			return null;
+		}
+
+		[SharpOS.AOT.Attributes.LabelledAlloc]
+		public unsafe static byte* LabelledAlloc (string label, uint value)
+		{
+			return null;
+		}
+
+		[SharpOS.AOT.Attributes.LabelAddress]
+		public unsafe static uint GetFunctionPointer (string label)
+		{
+			return 0;
+		}
+		#endregion
+	}
+}
+
