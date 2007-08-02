f23482437af2c49fb78903baca2dfb56f121d286
Bruce Markham <illuminus86@gmail.com>
2007-08-02 04:56:31 +0000

    deleted files

--- kernel/SharpOS.Kernel.Core/RebuildAndRun.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cmd /C "Rebuild.bat nopause"
-if not errorlevel 0 exit
-cd ..\build
-cmd /C "CreateImageAndRunOnBochs.bat nopause"
-if not errorlevel 0 exit
-IF not "%1"=="nopause" pause
-exit
