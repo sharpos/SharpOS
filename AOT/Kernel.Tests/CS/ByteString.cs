32c8c86c7d175d23e9cd65363e73e98548484031
William Lahti <xfurious@gmail.com>
2007-08-04 13:38:14 +0000

    few more tests

---
+++ AOT/Kernel.Tests/CS/ByteString.cs
@@ -0,0 +1,35 @@
+//
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	William Lahti <xfurious@gmail.com>
+//
+// Licensed under the terms of the GNU GPL License version 2.
+//
+
+using SharpOS;
+using SharpOS.AOT.Attributes;
+
+namespace SharpOS.Kernel.Tests.CS {
+	public unsafe class ByteString {
+
+		/// <summary>
+		/// </summary>
+		public static uint CMP0 ()
+		{
+			byte *str = String ("XYZ");
+			uint result = 1;
+			
+			if (str[0] != 'X' || str[1] != 'Y' || str[2] != 'Z')
+				result = 0;
+			
+			return result;
+		}
+
+		[String]
+		public static byte *String (string str)
+		{
+			return null;
+		}
+	}
+}
---
+++ AOT/Kernel.Tests/CS/StaticConstructor.cs
@@ -0,0 +1,27 @@
+//
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	William Lahti <xfurious@gmail.com>
+//
+// Licensed under the terms of the GNU GPL License version 2.
+//
+
+namespace SharpOS.Kernel.Tests.CS {
+	public unsafe class StaticConstructor {
+		static StaticConstructor ()
+		{
+			result = 1;
+		}
+
+		static uint result = 0;
+
+		/// <summary>
+		/// tests the AOT's handling of a static constructor
+		/// </summary>
+		public static uint CMPStaticConstructor ()
+		{
+			return result;
+		}
+	}
+}
