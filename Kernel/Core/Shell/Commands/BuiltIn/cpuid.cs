5b6fa9eb2d510447b143c74027e848672610fe2e
Jonathan Dickinson <jonathand.za@gmail.com>
2007-12-28 20:08:52 +0000

    many fun commands

---
+++ Kernel/Core/Shell/Commands/BuiltIn/cpuid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpOS.AOT.Attributes;
+using SharpOS.Foundation;
+using SharpOS.ADC;
+
+namespace SharpOS.Shell.Commands.BuiltIn
+{
+    public unsafe static class cpuid
+    {
+        public const string name = "cpuid";
+        public const string shortDescription = "Displays info about the cpu.";
+        public const string lblExecute = "COMMANDS.cpu.Execute";
+        public const string lblGetHelp = "COMMANDS.cpu.GetHelp";
+
+        [Label( lblExecute )]
+        public static void Execute( CommandExecutionContext* context )
+        {
+            // ARCHDEPENDS: X86
+            SharpOS.ADC.X86.CPU.WriteProcessorInfo( );
+        }
+
+        [Label( lblGetHelp )]
+        public static void GetHelp( CommandExecutionContext* context )
+        {
+            TextMode.WriteLine( "Syntax: " );
+            TextMode.WriteLine( "     cpuid" );
+            TextMode.WriteLine( "" );
+            TextMode.WriteLine( "Gets information about the CPU." );
+        }
+
+        public static CommandTableEntry* CREATE( )
+        {
+            CommandTableEntry* entry = (CommandTableEntry*) SharpOS.ADC.MemoryManager.Allocate( (uint) sizeof( CommandTableEntry ) );
+
+            entry->name = (CString8*) SharpOS.Stubs.CString( name );
+            entry->shortDescription = (CString8*) SharpOS.Stubs.CString( shortDescription );
+            entry->func_Execute = (void*) SharpOS.Stubs.GetLabelAddress( lblExecute );
+            entry->func_GetHelp = (void*) SharpOS.Stubs.GetLabelAddress( lblGetHelp );
+            entry->nextEntry = null;
+
+            return entry;
+        }
+    }
+}
---
+++ Kernel/Core/Shell/Commands/BuiltIn/victim.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpOS.AOT.Attributes;
+using SharpOS.Foundation;
+using SharpOS.ADC;
+
+namespace SharpOS.Shell.Commands.BuiltIn
+{
+    public unsafe static class victim
+    {
+        private const string name = "victim";
+        private const string shortDescription = "This is a placeholder for a bug.";
+        private const string lblExecute = "COMMANDS.victim.Execute";
+        private const string lblGetHelp = "COMMANDS.victim.GetHelp";
+
+        [Label( lblExecute )]
+        public static void Execute( CommandExecutionContext* context )
+        {
+
+        }
+
+        [Label( lblGetHelp )]
+        public static void GetHelp( CommandExecutionContext* context )
+        {
+
+        }
+
+        public static CommandTableEntry* CREATE( )
+        {
+            CommandTableEntry* entry = (CommandTableEntry*) SharpOS.ADC.MemoryManager.Allocate( (uint) sizeof( CommandTableEntry ) );
+
+            entry->name = (CString8*) SharpOS.Stubs.CString( name );
+            entry->shortDescription = (CString8*) SharpOS.Stubs.CString( shortDescription );
+            entry->func_Execute = (void*) SharpOS.Stubs.GetLabelAddress( lblExecute );
+            entry->func_GetHelp = (void*) SharpOS.Stubs.GetLabelAddress( lblGetHelp );
+
+            return entry;
+        }
+    }
+}
