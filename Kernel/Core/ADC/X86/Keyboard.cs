7e9fb187ab794c19d265b2f8578b454b8d991004
Jonathan Dickinson <jonathand.za@gmail.com>
2007-12-30 19:59:13 +0000

    fixed the CapsLock issue!!! check the sendcommand(keyboardcommands) and the keyboardmessages enum.

--- Kernel/Core/ADC/X86/Keyboard.cs
+++ Kernel/Core/ADC/X86/Keyboard.cs
@@ -105,6 +105,7 @@ namespace SharpOS.Kernel.ADC.X86
 		enum KeyboardMessages
 		{
 			Too_Many_Keys			= 0x00,	// Too many keys are being pressed at once
+                        Unknown                         = 0x3a, // This is causing our CAPS issue.
 			Basic_Assurance_Test		= 0xaa,
 			Echo_Command_Result		= 0xee,
 			Acknowledge			= 0xfa,	// Sent by every command, except eeh and feh
@@ -143,14 +144,16 @@ namespace SharpOS.Kernel.ADC.X86
 
 				message = (KeyboardMessages)IO.In8 (IO.Port.KB_data_port);
 
-				if (message == KeyboardMessages.Request_Resend) 
-					continue; 
-				else if (message == KeyboardMessages.Acknowledge)
-					return;
-				else {
-					Diagnostics.Error ("ADC.X86.Keyboard.SendCommand(): unhandled message");
-					return;
-				}
+                                if (message == KeyboardMessages.Request_Resend)
+                                        continue;
+                                else if (message == KeyboardMessages.Acknowledge)
+                                        return;
+                                else if (message == KeyboardMessages.Unknown) // This was the cause of the caps issue.
+                                        return;
+                                else {
+                                        Diagnostics.Error ("ADC.X86.Keyboard.SendCommand(): unhandled message");
+                                        return;
+                                }
 				
 			} while (message != KeyboardMessages.Acknowledge);
 		}
