42b5ec7767f17184c76562dbd6d192d7831dd11e
Sander van Rossen <sander.vanrossen@gmail.com>
2008-01-04 12:56:10 +0000

    added interlocked

---
+++ Kernel/Core/ADC/Interlocked.cs
@@ -0,0 +1,47 @@
+// 
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Sander van Rossen <sander.vanrossen@gmail.com>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+using System;
+using SharpOS;
+using SharpOS.AOT.X86;
+using SharpOS.AOT.IR;
+using AOTAttr = SharpOS.AOT.Attributes;
+
+namespace SharpOS.Kernel.ADC
+{
+	public static unsafe class Interlocked {
+		/* .. causes error in AOT ..
+		#region Add
+		[AOTAttr.ADCStub]
+		public static unsafe uint Add(uint* location, uint value) { throw new Exception("not implemented"); }
+		#endregion
+		*/
+
+		#region CompareExchange
+		[AOTAttr.ADCStub]
+		public static unsafe uint CompareExchange(uint* location, uint value, uint comparand) { throw new Exception("not implemented"); }
+		#endregion
+
+		#region Decrement
+		[AOTAttr.ADCStub]
+		public static unsafe uint Decrement(uint* location) { throw new Exception("not implemented"); }
+		#endregion
+
+		#region Exchange
+		[AOTAttr.ADCStub]
+		public static unsafe uint Exchange(uint* location, uint value) { throw new Exception("not implemented"); }
+		#endregion
+
+		#region Increment
+		[AOTAttr.ADCStub]
+		public static unsafe uint Increment(ref uint location) { throw new Exception("not implemented"); }
+		#endregion
+	}
+}
---
+++ Kernel/Core/ADC/X86/Interlocked.cs
@@ -0,0 +1,71 @@
+//
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Sander van Rossen <sander.vanrossen@gmail.com>
+//
+// Licensed under the terms of the GNU GPL License version 2.
+//
+
+using SharpOS.AOT;
+using SharpOS.AOT.X86;
+using System;
+
+namespace SharpOS.Kernel.ADC.X86
+{
+	public static class Interlocked	{
+		/* .. causes error in AOT ..
+		#region Add
+		public static unsafe uint Add(uint* location, uint value)
+		{
+			Asm.LOCK();
+			Asm.ADD(location, value);
+			return *location;
+		}
+		#endregion
+		*/
+		  
+		#region CompareExchange
+		public static unsafe uint CompareExchange(uint* location, uint value, uint comparand)
+		{
+			Asm.MOV(R32.ECX, location);
+			Asm.MOV(R32.EDX, &value);
+			Asm.MOV(R32.EAX, &comparand);
+			Asm.LOCK();
+			Asm.CMPXCHG(R32.ECX, R32.EDX);
+			Asm.MOV(location, R32.EAX);
+			return *location;
+		}
+		#endregion
+
+		#region Decrement
+		public static unsafe uint Decrement(uint* location)
+		{
+			Asm.LOCK();
+			Asm.DEC(location);
+			return *location;
+		}
+		#endregion
+
+		#region Exchange
+		public static unsafe uint Exchange(uint* location, uint value)
+		{
+			Asm.MOV(R32.ECX, location);
+			Asm.MOV(R32.EDX, &value);
+			Asm.LOCK();
+			Asm.XCHG(R32.ECX, R32.EDX);
+			Asm.MOV(location, R32.EDX); //? a guess
+			return *location;
+		}
+		#endregion
+
+		#region Increment
+		public static unsafe uint Increment(uint* location)
+		{
+			Asm.LOCK();
+			Asm.INC(location);
+			return *location;
+		}
+		#endregion
+	}
+}
