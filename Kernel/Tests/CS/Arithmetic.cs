aaf667938c5bbf38b71ad3182060a049d4ee2149
William Lahti <xfurious@gmail.com>
2008-01-10 19:22:44 +0000

    Added 64-bit arithmetic testcases

--- Kernel/Tests/CS/Arithmetic.cs
+++ Kernel/Tests/CS/Arithmetic.cs
@@ -3,10 +3,13 @@
 //
 // Authors:
 //	Mircea-Cristian Racasan <darx_kies@gmx.net>
+//	William Lahti <xfurious@gmail.com>
 //
 // Licensed under the terms of the GNU GPL v3,
 //  with Classpath Linking Exception for Libraries
 //
+// TODO:
+// - Create testcases for 64-bit division when implemented
 
 namespace SharpOS.Kernel.Tests.CS {
 	public class Arithmetic {
@@ -30,7 +33,29 @@ namespace SharpOS.Kernel.Tests.CS {
 			return a / b;
 		}
 
-		public static uint CMPSimpleAdd ()
+		public static ulong Add (ulong a, ulong b)
+		{
+			return a + b;
+		}
+
+		public static ulong Subtract (ulong a, ulong b)
+		{
+			return a - b;
+		}
+
+		public static ulong Multiply (ulong a, ulong b)
+		{
+			return a * b;
+		}
+
+		public static ulong Divide (ulong a, ulong b)
+		{
+			// FIXME: 64-bit division not implemented yet
+			// return a / b;
+			return a;
+		}
+
+		public static uint CMPSimpleAdd32 ()
 		{
 			if (Add (1, 2) == 3)
 				return 1;
@@ -38,7 +63,7 @@ namespace SharpOS.Kernel.Tests.CS {
 			return 0;
 		}
 
-		public static uint CMPSimpleSubtract ()
+		public static uint CMPSimpleSubtract32 ()
 		{
 			if (Subtract (2, 1) == 1)
 				return 1;
@@ -46,7 +71,7 @@ namespace SharpOS.Kernel.Tests.CS {
 			return 0;
 		}
 
-		public static uint CMPSimpleMultiply ()
+		public static uint CMPSimpleMultiply32 ()
 		{
 			if (Multiply (4, 4) == 16)
 				return 1;
@@ -54,9 +79,60 @@ namespace SharpOS.Kernel.Tests.CS {
 			return 0;
 		}
 
-		public static uint CMPSimpleDivide ()
+		public static uint CMPSimpleDivide32 ()
 		{
 			if (Divide (10, 5) == 2)
+				return 1;
+
+			return 0;
+		}
+
+		public static uint CMPSimpleAdd64 ()
+		{
+			if (Add (1L, 2L) == 3L)
+				return 1;
+
+			return 0;
+		}
+
+		public static uint CMPSimpleSubtract64 ()
+		{
+			if (Subtract (2L, 1L) == 1L)
+				return 1;
+
+			return 0;
+		}
+
+		public static uint CMPSimpleMultiply64 ()
+		{
+			if (Multiply (4L, 4L) == 16L)
+				return 1;
+
+			return 0;
+		}
+
+		public static uint CMPOverflowAdd64 ()
+		{
+			if (Add (1000000000000000L, 10000000000000L) ==
+					1010000000000000L)
+				return 1;
+
+			return 0;
+		}
+
+		public static uint CMPOverflowSubtract64 ()
+		{
+			if (Subtract (1000000000000000L, 10000000000000L) ==
+					990000000000000L)
+				return 1;
+
+			return 0;
+		}
+
+		public static uint CMPOverflowMultiply64 ()
+		{
+			if (Multiply (1000000000000000L, 100L) ==
+					100000000000000000L)
 				return 1;
 
 			return 0;
