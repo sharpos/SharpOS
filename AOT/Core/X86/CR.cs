465c49e4f392035389543c9a05b3beb5e9dcb133
Darx Kies <darxkies@gmail.com>
2008-01-21 10:26:08 +0000

    Added missing CR0Flags.

--- AOT/Core/X86/CR.cs
+++ AOT/Core/X86/CR.cs
@@ -18,6 +18,24 @@ using Mono.Cecil.Cil;
 using Mono.Cecil.Metadata;
 
 namespace SharpOS.AOT.X86 {
+	[System.Flags]
+	public enum CR0Flags : uint {
+		PE = (1U << 0),		// protected mode
+		MP = (1U << 1),		// math present
+		EM = (1U << 2),		// emulate numeric extension
+		TS = (1U << 3),		// task switched
+		ET = (1U << 4),		// extension type
+		NE = (1U << 5),		// numeric error enable
+
+		WP = (1U << 16),	// write protect
+
+		AM = (1U << 18),	// alignment mask
+
+		NW = (1U << 29),	// not write-through
+		CD = (1U << 30),	// cache disable
+		PG = (1U << 31)		// paging enable
+	}
+
 	public class CR {
 
 		public static readonly CRType CR0 = new CRType ("CR0", 0);
