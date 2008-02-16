ca6075e36c21c94b50d65b84b2079a9db1332f72
William Lahti <xfurious@gmail.com>
2008-02-16 20:14:53 +0000

    and the cleanup. so much for the perfect ccnet record :-P

--- Kernel/Core/ADC/MemoryUtil.cs
+++ Kernel/Core/ADC/MemoryUtil.cs
@@ -17,62 +17,51 @@ namespace SharpOS.Kernel.ADC {
 		[AOTAttr.ADCStub]
 		public unsafe static void Call (void* functionPointer, void* pointeredParameter)
 		{
-			Diagnostics.Error ("Unimplemented - MemoryUtil.Call");
 		}
 
-        [AOTAttr.ADCStub]
-        public unsafe static void MemSet(uint value, uint dst, uint count)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public unsafe static void MemSet(uint value, uint dst, uint count)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public unsafe static void MemSet32(uint value, uint dst, uint count)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public unsafe static void MemSet32(uint value, uint dst, uint count)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public static uint BitCount(byte value)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public static uint BitCount(byte value)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public static uint BitCount(ushort value)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public static uint BitCount(ushort value)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public static uint BitCount(uint value)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public static uint BitCount(uint value)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public static uint BitCount(ulong value)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public static uint BitCount(ulong value)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public unsafe static void MemCopy(uint src, uint dst, uint count)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public unsafe static void MemCopy(uint src, uint dst, uint count)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public unsafe static void MemCopy32 (uint src, uint dst, uint count)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public unsafe static void MemCopy32 (uint src, uint dst, uint count)
+		{
+		}
 
-        [AOTAttr.ADCStub]
-        public unsafe static void Call (uint address, uint value)
-        {
-        }
-
-        [AOTAttr.ADCStub]
-        public unsafe static void Call (void* address, uint value)
-        {
-        }
-
-        [AOTAttr.ADCStub]
-        public unsafe static void Call(void* functionPointer, void* pointeredParameter)
-        {
-        }
+		[AOTAttr.ADCStub]
+		public unsafe static void Call (uint address, uint value)
+		{
+		}
 	}
 }
