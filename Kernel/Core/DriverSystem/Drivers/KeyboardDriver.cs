b04c34ec3a63aaa99bf41549f0394eef946f0c6c
Sander van Rossen <sander.vanrossen@gmail.com>
2008-02-25 15:07:35 +0000

    yet more changes

---
+++ Kernel/Core/DriverSystem/Drivers/KeyboardDriver.cs
@@ -0,0 +1,30 @@
+﻿// 
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Sander van Rossen <sander.vanrossen@gmail.com>
+//	William Lahti <xfurious@gmail.com>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+using System;
+
+namespace SharpOS.Kernel.ADC.X86 {
+	public class KeyboardDriver : GenericDriver {
+
+		private IOPortStream ControllerCommands;
+		private IOPortStream DataPort;
+
+		#region Initialize
+		public override bool Initialize(IDevice device, IHardwareResourceManager manager)
+		{
+			ControllerCommands	= manager.Request8bitIOPort((ushort)IO.Port.KB_controller_commands);
+			DataPort			= manager.Request8bitIOPort((ushort)IO.Port.KB_data_port);
+			
+			return (isInitialized = false);
+		}
+		#endregion
+	}
+}
---
+++ Kernel/Core/DriverSystem/Drivers/LPCBusDriver.cs
@@ -0,0 +1,56 @@
+﻿// 
+// (C) 2006-2007 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Sander van Rossen <sander.vanrossen@gmail.com>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using SharpOS.Kernel;
+using SharpOS.AOT.X86;
+using SharpOS.AOT.IR;
+
+namespace SharpOS.Kernel.ADC.X86 {
+
+	/// <summary>
+	/// Low Pin Count bus
+	///  The LPC bus was introduced by Intel in 1998 as a substitute for 
+	///  the Industry Standard Architecture (ISA) bus.
+	/// </summary>
+	/// <TODO>LPCBusDriver should use implementations of LPCDevice/LPCDriver class internally</TODO>
+	public class LPCBusDriver : GenericDriver	{
+
+		// LPC devices
+		//    FloppyDiskControllers
+		//    FloppyDiskDrives
+		//    Parallel ports
+		//    Serial ports
+
+		#region Initialize
+		public override bool Initialize(IDevice device, IHardwareResourceManager manager)
+		{
+			return (isInitialized = false);
+		}
+		#endregion
+		
+		#region SubDevices
+		public override bool HasSubDevices
+		{
+			get { return true; }
+		}
+
+		public override bool GetSubDevices(out IDevice[] subDevices)
+		{
+			subDevices = new IDevice[] {
+			};
+			return false;
+		}
+		#endregion
+	}
+}
