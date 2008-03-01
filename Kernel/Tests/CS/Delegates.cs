946562f1b60d2bab24db1681204b87bce57d060d
Stanislaw Pitucha <viraptor@gmail.com>
2008-03-01 20:51:33 +0000

    Commented out delegate test that should work later.

---
+++ Kernel/Tests/CS/Delegates.cs
@@ -0,0 +1,29 @@
+//
+// (C) 2008 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Stanisław Pitucha <viraptor@gmail.com>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+namespace SharpOS.Kernel.Tests.CS {
+/*	public class Delegates {
+		delegate int NoArgsDelegate ();
+		
+		static int Function ()
+		{
+			return 69;
+		}
+
+		public static uint CMPBasic ()
+		{
+			NoArgsDelegate p = new NoArgsDelegate (Function);
+
+			if (p() == 69)
+				return 1;
+			return 0;
+		}
+	}*/
+}
