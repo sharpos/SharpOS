56f5d3852341c6be0e02371f7ada14155c42bb9b
Phil Garcia <phil@thinkedge.com>
2008-04-11 06:21:37 +0000

    * Added GPL Copyright.

--- Kernel/Core/Korlib/System/IO/FileShare.cs
+++ Kernel/Core/Korlib/System/IO/FileShare.cs
@@ -1,3 +1,12 @@
+//
+// (C) 2006-2008 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Michael Ruck (aka grover) <sharpos@michaelruck.de>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
 
 using SharpOS.AOT.Attributes;
 
--- Kernel/Core/Korlib/System/IO/Path.cs
+++ Kernel/Core/Korlib/System/IO/Path.cs
@@ -1,8 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+//
+// (C) 2006-2008 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Michael Ruck (aka grover) <sharpos@michaelruck.de>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+using SharpOS.AOT.Attributes;
 
 namespace InternalSystem.IO {
+	[TargetNamespace ("System.IO")]
 	public static class Path {
 		public static readonly char AltDirectorySeparatorChar = '\\';
 		public static readonly char DirectorySeparatorChar = '/';
