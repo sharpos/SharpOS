02087f3f307cf43d1f0728f2e770872f07efad5d
Michael Ruck <sharpos@michaelruck.de>
2008-04-13 21:47:59 +0000

    Renaming Directory to DirectoryNode Renaming SymbolicLink to SymbolicLinkNode

---
+++ Kernel/Core/Vfs/DirectoryNode.cs
@@ -0,0 +1,79 @@
+//
+// (C) 2006-2008 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Michael Ruck (aka grover) <sharpos@michaelruck.de>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+using System;
+using InternalSystem.Collections;
+using System.IO;
+
+
+namespace SharpOS.Kernel.Vfs
+{
+	/// <summary>
+	/// Represents a basic directory in the VFS namespace.
+	/// </summary>
+	/// <remarks>
+	/// This class can be inherited if necessary to provide specialized
+	/// directory handling if required.
+	/// </remarks>
+	public class DirectoryNode : NodeBase
+	{
+
+		#region Data members
+
+		/// <summary>
+		/// Holds all nodes added to the root vfs node.
+		/// </summary>
+		private ArrayList _nodes;
+
+		#endregion // Data members
+
+		#region Construction
+
+		/// <summary>
+		/// Initializes a new instance of the DirectoryNode object.
+		/// </summary>
+		/// <param name="fs">The filesystem, which owns the node.</param>
+		public DirectoryNode (IFileSystem fs)
+			: base (fs, VfsNodeType.Directory)
+		{
+			_nodes = new ArrayList ();
+		}
+
+		#endregion // Construction
+
+		#region IVfsNode members
+
+		public override IVfsNode Create (char[] name, VfsNodeType nodeType, object settings)
+		{
+			// FIXME: throw new NotImplementedException();
+			return null;
+		}
+
+		public override IVfsNode Lookup (char[] name)
+		{
+			// FIXME: Lookup the node in the members
+			return null;
+		}
+
+		public override object Open (FileAccess access, FileShare sharing)
+		{
+			// FIXME: return something like: new System.IO.DirectoryInfo(VirtualFileSystem.GetPath(this));
+			//throw new NotImplementedException();
+			return null;
+		}
+
+		public override void Delete ()
+		{
+			// FIXME: throw new NotImplementedException();
+		}
+
+		#endregion // IVfsNode members
+	}
+}
---
+++ Kernel/Core/Vfs/SymbolicLinkNode.cs
@@ -0,0 +1,76 @@
+//
+// (C) 2006-2008 The SharpOS Project Team (http://www.sharpos.org)
+//
+// Authors:
+//	Michael Ruck (aka grover) <sharpos@michaelruck.de>
+//
+// Licensed under the terms of the GNU GPL v3,
+//  with Classpath Linking Exception for Libraries
+//
+
+using System;
+using System.IO;
+
+namespace SharpOS.Kernel.Vfs {
+	public sealed class SymbolicLinkNode : NodeBase {
+		#region Data members
+
+		/// <summary>
+		/// The target of the symbolic link.
+		/// </summary>
+		private char[] _target;
+
+		#endregion // Data members
+
+		#region Construction
+
+		public SymbolicLinkNode (IFileSystem fs, char[] target)
+			: base (fs, VfsNodeType.SymbolicLink)
+		{
+			_target = target;
+		}
+
+		#endregion // Construction
+
+		#region Properties
+
+		/// <summary>
+		/// Retrieves the target path of the symbolic link.
+		/// </summary>
+		public char[] Target
+		{
+			get
+			{
+				return _target;
+			}
+		}
+
+		#endregion // Properties
+
+		#region IVfsNode Members
+
+		public override IVfsNode Create (char[] name, VfsNodeType type, object settings)
+		{
+			// Pass this request to the link target node.
+			// FIXME: throw new NotImplementedException();
+			return null;
+		}
+
+		public override object Open(FileAccess access, FileShare sharing)
+		{
+			// FIXME:
+			// - Pass this request to the link target node?
+			// - Do we really want this?
+			// FIXME: throw new NotImplementedException();
+			return null;
+		}
+
+		public override void Delete()
+		{
+			// FIXME: Delete the symbolic link from the filesystem, after all names have been dropped.
+			// throw new NotImplementedException();
+		}
+
+		#endregion // IVfsNode Members
+	}
+}
